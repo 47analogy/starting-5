c48ca2fdf5b39d2c268a724050ab53378bb8172e
analogy47 <actweed47@gmail.com>
2019-03-10 13:31:33 -0400

    add NavBar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import TeamList from './components/TeamList/TeamList';
+import NavBar from './components/NavBar/NavBar';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<NavBar />
 				<TeamList />
 			</div>
 		);
